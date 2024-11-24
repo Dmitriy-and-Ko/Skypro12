8073769cc27053a9c231dc9e3083798e2ed0090d
Dmitriy Kovshov <dmitriykovshov32@gmail.com>
2024-11-24 15:00:37 +0300

    test_masks fixes

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,4 @@
+
 from typing import Union
 
 
@@ -5,11 +6,18 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция возвращает замаскированный номер карты в формате XXXX XX** **** XXXX, где X - цифра"""
     if len(card_number) != 16:
         return 'Неправильно задан номер карты. Проверьте номер карты должен содержать 16 цифр'
+    elif card_number.isdigit() == False:
+        return 'Номер карты должен содержать только цифры'
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
     """Функция возвращает замаскированный номер счёта в формате **ХХХХ, где XXXX, последние 4 цифры номера"""
-
-    mask_account_number = f"**{account_number[-5:-1]}"
+    if len(account_number) != 20:
+        return 'Неправильно задан номер счёта. Проверьте номер счёта должен содержать 20 цифр'
+    elif account_number.isdigit() == False:
+        return 'Номер счёта должен содержать только цифры'
+    mask_account_number = f"**{account_number[-4:]}"
     return mask_account_number
+
+
---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.fixture()
+def standart_numbers_for_get_masks_card_numbers():
+    return '3355 89** **** 0921'
+
+@pytest.fixture()
+def not_standart_numbers_for_get_masks_card_numbers():
+    return 'Неправильно задан номер карты. Проверьте номер карты должен содержать 16 цифр'
+
+@pytest.fixture()
+def if_card_number_with_letters():
+    return 'Номер карты должен содержать только цифры'
+
+@pytest.fixture()
+def standart_number_for_get_masks_account():
+    return '**1849'
+
+@pytest.fixture()
+def not_standart_number_for_get_masks_account():
+    return 'Неправильно задан номер счёта. Проверьте номер счёта должен содержать 20 цифр'
+
+@pytest.fixture()
+def if_account_number_contain_letters():
+    return 'Номер счёта должен содержать только цифры'
