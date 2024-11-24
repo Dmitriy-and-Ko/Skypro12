4e290709f41e1c8f08bc8dc8ef7f9bbbe2718021
Dmitriy Kovshov <dmitriykovshov32@gmail.com>
2024-11-24 17:38:51 +0300

    test_mask_account_card fixes

--- src/widget.py
+++ src/widget.py
@@ -3,20 +3,24 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(data_bank: str) -> str:
     """Фцнкция принимает и маскирует полностью как карту банка, так и номер счёта"""
-    alpha_part_data_bank = ""
-    digit_part_data_bank = ""
-    list_card = ["visa", "maestro", "mastercard", "visaclassic", "visaplatinum", "visagold"]
-    list_bild = ["счёт", "счет"]
-    for element in data_bank:
-        if element.isalpha():
-            alpha_part_data_bank += element
-        elif element.isdigit():
-            digit_part_data_bank += element
 
-    if (alpha_part_data_bank.lower() in list_card) and (len(digit_part_data_bank) == 16):
-        return f"{alpha_part_data_bank} {get_mask_card_number(digit_part_data_bank)}"
-    elif (alpha_part_data_bank.lower() in list_bild) and len(digit_part_data_bank) == 20:
-        return f"{alpha_part_data_bank} {get_mask_account(digit_part_data_bank)}"
+    list_card = ["visa", "maestro", "master card", "visa classic", "visa platinum", "visa gold"]
+    list_bild = ["счёт", "счет"]
+    list_data_bank = data_bank.split()
+    list_alpha_part_data_bank = list(filter(lambda x: x.isalpha(), list_data_bank))
+    list_digit_part_data_bank = list(filter(lambda x: x.isdigit(), list_data_bank))
+    string_alpha_part_data_bank = ' '.join(list_alpha_part_data_bank)
+    string_digit_part_data_bank = ''.join(list_digit_part_data_bank)
+
+
+    if (string_alpha_part_data_bank.lower() in list_card) and (len(string_digit_part_data_bank) == 16):
+        return f"{string_alpha_part_data_bank} {get_mask_card_number(string_digit_part_data_bank)}"
+    elif (string_alpha_part_data_bank.lower() in list_bild) and len(string_digit_part_data_bank) == 20:
+        return f"{string_alpha_part_data_bank} {get_mask_account(string_digit_part_data_bank)}"
+    elif len(string_alpha_part_data_bank) == 0 and len(string_digit_part_data_bank) == 16:
+        return 'Вы не ввели тип карты. Укажите тип карты'
+    elif len(string_alpha_part_data_bank) == 0 and len(string_digit_part_data_bank) == 20:
+        return 'Вы неправильно ввели счёт. Убедитесь, что при вводе перед номером указано слово Счёт'
     else:
         return "Вы ввели некорректный тип карты / счёта, или карта / счёт содержит неверное количество цифр"
 
