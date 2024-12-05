766b1865d9348d57dea1dcd553623ba487c8d5a2
Dmitriy Kovshov <dmitriykovshov32@gmail.com>
2024-12-05 12:44:37 +0300

    fixes decorator without Error check

--- src/decorators.py
+++ src/decorators.py
@@ -1,14 +1,24 @@
 import os
 from functools import wraps
 
+# ROOT_DIR = Path(__file__).parent
+# print(ROOT_DIR)
+# PATH_TO_FILE = Path(ROOT_DIR, "data", "log_scripts.txt")
 PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "data", "log_scripts.txt")
-# def log(filename):
-#     def user_decor(func):
-#         @wraps(func)
-#         def inner(*args, **kwargs):
-#             result = func(*args, **kwargs)
-#             try:
-#                 if filename is not None:
-#                     with open("log_scripts")
-file = open(PATH_TO_FILE, 'r')
-print(file)
+def log(filename):
+    def user_decor(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if filename is not None:
+                with (open(PATH_TO_FILE, 'a', encoding="utf-8") as file):
+                    return file.write(result)
+            else:
+                return result
+        return inner
+    return user_decor
+
+# print(PATH_TO_FILE)
+# with (open(PATH_TO_FILE, 'a', encoding="utf-8") as file):
+#     content = "Учись работать с файлами. \n"
+#     file.write(content)
