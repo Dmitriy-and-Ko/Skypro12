5f2404ca48a44fc8b511e3cc77371c717e0c407d
Dmitriy Kovshov <dmitriykovshov32@gmail.com>
2024-12-24 18:31:08 +0300

    get_list_with_request_string fixes

--- src/handler_list_tansactions.py
+++ src/handler_list_tansactions.py
@@ -1,10 +1,13 @@
 import re
 
+
 def get_list_with_request_string(list_transactions: list, request_string: str) -> list:
     """Функция, принимает список словарей с данными о банковских операциях и строку поиска.
     Возвращает список словарей, у которых в описании есть данная строка."""
     function_list = list()
-    for elment in list_transactions:
-        if request_string in elment['description']:
-            function_list.append(elment)
+    pattern = re.compile(request_string)
+    for element in list_transactions:
+        if element.get('description') and pattern.search(element['description']):
+            function_list.append(element)
     return function_list
+
